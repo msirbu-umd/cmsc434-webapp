13f940767b32ffd3b885ffbbc496f38cfdc54b46
cindyxinren <cindyxinren@mgmail.com>
2016-05-01 13:13:19 -0400

    change maplanding to inverse colors

--- usa_voterID_map.js
+++ usa_voterID_map.js
@@ -1,26 +1,31 @@
 $(function () {
 
-		var data = Highcharts.geojson(Highcharts.maps['countries/us/us-all']),
-		// Some responsiveness
+    var data = Highcharts.geojson(Highcharts.maps['countries/us/us-all']),
+    // Some responsiveness
         small = $('#container').width() < 400;
-        
-		//console.log(this.properties); 
-		 $.each(data, function (i) {
+
+    //console.log(this.properties);
+    $.each(data, function (i) {
         this.drilldown = this.properties['hc-key'];
-		});
-		//console.log(data); 
-		
-		//spacingBottom: 20
+    });
+    //console.log(data);
+
+    //spacingBottom: 20
     // Instanciate the map
     $('#container').highcharts('Map', {
-		
-		
-        title : {
-                text : 'VOTER ID LAWS'
-            },
-						subtitle: {
-                        text: 'Source: <a href="http://www.ncsl.org/research/elections-and-campaigns/voter-id.aspx">NCSL</a>'
-                    },
+
+        chart: {
+            backgroundColor: '#272b30'
+        },
+        title: {
+            text: 'VOTER ID LAWS',
+            style: {
+                color: '#F0F8FF'
+            }
+        },
+        subtitle: {
+            text: 'Source: <a href="http://www.ncsl.org/research/elections-and-campaigns/voter-id.aspx">NCSL</a>'
+        },
 
         legend: {
             enabled: true
@@ -45,54 +50,52 @@ $(function () {
                 }
 
             },
-						series: {
+            series: {
                 cursor: 'pointer',
                 point: {
                     events: {
                         click: function () {
-												   //console.log(this); 
-													 //console.log(this.name); 
-                           location.href = this.name+"_statepage.html";
+                            //console.log(this);
+                            //console.log(this.name);
+                            location.href = this.name + "_statepage.html";
                         }
                     }
                 }
             }
         },
 
-				
-				
-				
-        series : [{
+
+        series: [{
             name: 'No document required to vote',
-						color: '#A1A1A1',
+            color: '#A1A1A1',
             data: $.map(['ME', 'VT', 'NJ', 'MA', 'MD', 'DC', 'NY', 'PA', 'WV',
-						'IL', 'IA', 'MN', 'NE', 'WY', 'NM', 'NV', 'CA', 'OR'], function (code) {
-                return { code: code };
+                'IL', 'IA', 'MN', 'NE', 'WY', 'NM', 'NV', 'CA', 'OR'], function (code) {
+                return {code: code};
             })
         }, {
             name: 'ID requested; photo not required',
-						color: '#8EFBBF',
+            color: '#8EFBBF',
             data: $.map(['AK', 'WA', 'MT', 'UT', 'CO', 'OK', 'MO', 'AR', 'KY',
-						'NC', 'SC', 'DE', 'NH', 'CT', 'RI'], function (code) {
-                return { code: code };
+                'NC', 'SC', 'DE', 'NH', 'CT', 'RI'], function (code) {
+                return {code: code};
             })
         }, {
             name: 'Photo ID requested',
-						color: '#32BF72', 
+            color: '#32BF72',
             data: $.map(['ID', 'SD', 'MI', 'LA', 'AL', 'FL', 'HI'], function (code) {
-                return { code: code };
+                return {code: code};
             })
         }, {
             name: 'Strict Non-Photo ID',
-						color: '#078841',
-            data: $.map(['AZ','OH'], function (code) {
-                return { code: code };
+            color: '#078841',
+            data: $.map(['AZ', 'OH'], function (code) {
+                return {code: code};
             })
-        },{
+        }, {
             name: 'Strict Photo ID',
-						color: '#044B24',
-            data: $.map(['TX','KS','ND','WI','IN','TN','MS','GA','VA'], function (code) {
-                return { code: code };
+            color: '#044B24',
+            data: $.map(['TX', 'KS', 'ND', 'WI', 'IN', 'TN', 'MS', 'GA', 'VA'], function (code) {
+                return {code: code};
             })
         }]
     });
